9b04b580f6579e2aac141e59197c1e2a3e7e7ddb
Yong Zhen Long <vividzhenlong@gmail.com>
2024-06-27 08:08:44 +0800

    Update Database Schema

--- db_schema.sql
+++ db_schema.sql
@@ -1,3 +1,4 @@
+-- SQLBook: Code
 
 -- This makes sure that foreign_key constraints are observed and that errors will be thrown for violations
 PRAGMA foreign_keys=ON;
@@ -6,29 +7,62 @@ BEGIN TRANSACTION;
 
 -- Create your tables with SQL commands here (watch out for slight syntactical differences with SQLite vs MySQL)
 
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL
+-- CREATE TABLE IF NOT EXISTS users (
+--     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_name TEXT NOT NULL
+-- );
+
+-- CREATE TABLE IF NOT EXISTS email_accounts (
+--     email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     email_address TEXT NOT NULL,
+--     user_id  INT, --the user that the email account belongs to
+--     FOREIGN KEY (user_id) REFERENCES users(user_id)
+-- );
+
+-- -- Insert default data (if necessary here)
+
+-- -- Set up three users
+-- INSERT INTO users ('user_name') VALUES ('Simon Star');
+-- INSERT INTO users ('user_name') VALUES ('Dianne Dean');
+-- INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
+
+-- -- Give Simon two email addresses and Diane one, but Harry has none
+-- INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
+-- INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
+-- INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
+
+-- Table: Accounts
+CREATE TABLE IF NOT EXISTS Author (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    blog_name TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS email_accounts (
-    email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email_address TEXT NOT NULL,
-    user_id  INT, --the user that the email account belongs to
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+-- Table: Articles
+CREATE TABLE IF NOT EXISTS Articles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER,
+    title TEXT NOT NULL,
+    content TEXT,
+    state TEXT CHECK(state IN ('draft', 'published')),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    published_at DATETIME,
+    number_of_reads INTEGER DEFAULT 0,
+    number_of_likes INTEGER DEFAULT 0,
+    FOREIGN KEY (author_id) REFERENCES Accounts(id)
 );
 
--- Insert default data (if necessary here)
+-- Table: ReaderComments
+CREATE TABLE IF NOT EXISTS ReaderComments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    article_id INTEGER,
+    commenter_name TEXT,
+    comment TEXT NOT NULL,
+    comment_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (article_id) REFERENCES Articles(id)
+);
 
--- Set up three users
-INSERT INTO users ('user_name') VALUES ('Simon Star');
-INSERT INTO users ('user_name') VALUES ('Dianne Dean');
-INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
-
--- Give Simon two email addresses and Diane one, but Harry has none
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
 
 COMMIT;
-
